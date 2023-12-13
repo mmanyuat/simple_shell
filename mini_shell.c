e74399fa9c9863ca0473d554172464ce7d20823f
mmanyuat <mmanyuat201@gmail.com>
2023-12-13 01:31:44 -0800

    all

--- mini_shell.c
+++ mini_shell.c
@@ -7,6 +7,50 @@
 #include <sys/wait.h>
 
 /**
+ * execute_cmd -function to exrcute the cmd
+ * @cmd:the command
+ * Return:nothing
+ */
+
+void execute_cmd(char *cmd)
+{
+	char **argv;
+	int status;
+	pid_t my_pid;
+
+	argv = print_token(cmd, " \t\n");
+	my_pid = fork();
+	if (my_pid == -1)
+	{
+	perror("fork_error");
+	free(argv);
+	_exit(EXIT_FAILURE);
+	}
+	if (my_pid == 0)
+	{
+	execve(argv[0], argv, NULL);
+	perror("error");
+	free(argv);
+	}
+	else
+	{
+	wait(&status);
+	if (WIFEXITED(status))
+	{
+	int exit_status = WEXITSTATUS(status);
+	if (exit_status != 0)
+	{
+		fprintf(stderr, "./shell: %s: Cmd failed %d\n", argv[0], exit_status);
+	}
+	}
+	else if (WIFSIGNALED(status))
+	{
+	fprintf(stderr, "./shell: %s: cmd failed %d\n", argv[0], WTERMSIG(status));
+	}
+	free(argv);
+	}
+}
+/**
  * main - function to build mini shell
  * Return: nothing
  */
@@ -26,29 +70,18 @@ int main(void)
 
 	write(1, message, strlen(message));
 	length = getline(&cmd, &n, stdin);
+	if (length == -1)
+	{
+	perror("getline");
+	free(cmd);
+	_exit(EXIT_FAILURE);
+	}
 	if (length == 1)
 	{
 	free(cmd);
-	exit(0);
+	_exit(0);
 	}
-	argv = print_token(cmd, " \t\n");
-	my_pid = fork();
-	if (my_pid == -1)
-	{
-	perror("error");
-	exit(EXIT_FAILURE);
-	}
-	if (my_pid == 0)
-	{
-	if (execve(argv[0], argv, NULL) == -1)
-	{
-	perror("error");
-	}
-	}
-	else
-	{
-	wait(&status);
-	}
+	execute_cmd(cmd);
 	}
 	free(argv);
 	free(cmd);
